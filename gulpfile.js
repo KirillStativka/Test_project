607fc8bff352582fbb36695dc51583bfa51942b7
gem install bundler <stativka291@gmail.com>
2017-08-03 17:03:35 +0300

    update pacckage

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp         = require('gulp'),
 		sass         = require('gulp-sass'),
 		autoprefixer = require('gulp-autoprefixer'),
-		cleanCSS    = require('gulp-clean-css'),
+		cleanCSS   	 = require('gulp-clean-css'),
 		rename       = require('gulp-rename'),
 		browserSync  = require('browser-sync').create(),
 		concat       = require('gulp-concat'),
